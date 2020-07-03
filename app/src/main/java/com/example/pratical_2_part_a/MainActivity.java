3133016cb513d2c29d3d27f9358f159ef5672095
PheyZhenWong <wongpz-wm19@student.tarc.edu.my>
2020-07-03 14:38:34 +0800

    commit by wong phey zhen

--- app/src/main/java/com/example/pratical_2_part_a/MainActivity.java
+++ app/src/main/java/com/example/pratical_2_part_a/MainActivity.java
@@ -2,29 +2,50 @@ package com.example.pratical_2_part_a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    private int mCount=0;
+    private TextView btnZero;
+    private TextView btnCount;
+    private int mCount = 0;
     private TextView mShowCount;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mShowCount = (TextView) findViewById(R.id.show_count);
+        btnCount = (TextView) findViewById(R.id.button_count);
+        btnZero = (TextView) findViewById(R.id.button_zero);
     }
-    public void showToast(View view){
-        Toast toast = Toast.makeText(this,"Hello AACS3353",Toast.LENGTH_SHORT);
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this,R.string.toast_message,Toast.LENGTH_SHORT);
 
         toast.show();
     }
-    public void countUp(View view){
+
+    @SuppressLint("SetTextI18n")
+    public void countUp(View view) {
         ++mCount;
         if(mShowCount != null)
             mShowCount.setText(Integer.toString(mCount));
+        btnZero.setBackgroundColor(Color.RED);
 
+        if((mCount%2)==0)
+            btnCount.setBackgroundColor(Color.BLUE);
+        else
+            btnCount.setBackgroundColor(Color.CYAN);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void showZero(View view){
+        btnZero.setBackgroundColor(Color.DKGRAY);
+        mCount=0;
+        mShowCount.setText(Integer.toString(mCount));
     }
 }
